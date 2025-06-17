1146c8acab031df294f87a8680843cccb9a9890e
antony <123041179+antony-raj22@users.noreply.github.com>
2025-06-17 19:18:02 +0530

    Update wormy.py

--- wormy/wormy.py
+++ wormy/wormy.py
@@ -1,7 +1,4 @@
-# Wormy (a Nibbles clone)
-# By Al Sweigart al@inventwithpython.com
-# http://inventwithpython.com/pygame
-# Released under a "Simplified BSD" license
+
 
 import random, pygame, sys
 from pygame.locals import *
